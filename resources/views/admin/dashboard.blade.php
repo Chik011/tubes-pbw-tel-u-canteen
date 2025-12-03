ffb2963a0fcf71490ab697cd7bdab7846591d7be
Chik011 <chikokasa11@gmail.com>
2025-12-03 18:22:06 +0700

    tes1

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="/">Landing page</a> <br>
+    <a href="/admin">Admin</a>
+
+    <h1>Ini Halaman Admin</h1>
+</body>
+</html>
