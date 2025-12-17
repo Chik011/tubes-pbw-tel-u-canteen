a023ba19f67dcafb33176750351795e927378b2c
fadhilahfajrin <fadhilahfajrin10300@gmail.com>
2025-12-17 13:39:34 +0700

    push

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -11,7 +11,7 @@
                         <div class="row">
                             <div class="col-md-6 mb-3">
                                 <div class="card">
-                                    <img src="{{ asset('img/1.png') }}" class="card-img-top" alt="Nasi Goreng">
+                                    <img src="{{ asset('img/naci_oyeng.jpeg') }}" class="card-img-top" alt="Nasi Goreng" height="300px";>
                                     <div class="card-body">
                                         <h5 class="card-title">Nasi Goreng</h5>
                                         <p class="card-text">Nasi goreng spesial dengan telur dan ayam.</p>
@@ -22,7 +22,7 @@
                             </div>
                             <div class="col-md-6 mb-3">
                                 <div class="card">
-                                    <img src="{{ asset('img/1.png') }}" class="card-img-top" alt="Ayam Bakar">
+                                    <img src="{{ asset('img/ayam_bakar.jpeg') }}" class="card-img-top" alt="Ayam Bakar" height="300px";>
                                     <div class="card-body">
                                         <h5 class="card-title">Ayam Bakar</h5>
                                         <p class="card-text">Ayam bakar dengan bumbu rempah.</p>
@@ -33,7 +33,7 @@
                             </div>
                             <div class="col-md-6 mb-3">
                                 <div class="card">
-                                    <img src="{{ asset('img/1.png') }}" class="card-img-top" alt="Sate Ayam">
+                                    <img src="{{ asset('img/sate_ayam.jpeg') }}" class="card-img-top" alt="Sate Ayam" height="300px";>
                                     <div class="card-body">
                                         <h5 class="card-title">Sate Ayam</h5>
                                         <p class="card-text">Sate ayam dengan bumbu kacang.</p>
@@ -44,7 +44,7 @@
                             </div>
                             <div class="col-md-6 mb-3">
                                 <div class="card">
-                                    <img src="{{ asset('img/1.png') }}" class="card-img-top" alt="Bakso">
+                                    <img src="{{ asset('img/bakso.jpeg') }}" class="card-img-top" alt="Bakso" height="300px";>
                                     <div class="card-body">
                                         <h5 class="card-title">Bakso</h5>
                                         <p class="card-text">Bakso daging sapi dengan mie.</p>
@@ -55,7 +55,7 @@
                             </div>
                             <div class="col-md-6 mb-3">
                                 <div class="card">
-                                    <img src="{{ asset('img/1.png') }}" class="card-img-top" alt="Mie Goreng">
+                                    <img src="{{ asset('img/mie_goreng.jpeg') }}" class="card-img-top" alt="Mie Goreng" height="300px";>
                                     <div class="card-body">
                                         <h5 class="card-title">Mie Goreng</h5>
                                         <p class="card-text">Mie goreng dengan sayuran dan telur.</p>
